2ebe6a47c764588772342fd0563a072d4b06f287
Jonathan Hosea <jonathan.hosea@me.com>
2014-10-15 12:28:57 +0700

    added index.php

---
+++ CMS/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>PT. Akuato Artha </title>
+</head>
+<body>
+<?php 
+	if (!empty ($_GET)) {
+	$admin = $_GET['admin'];
+	}
+
+	if (empty ($post))
+	{
+		echo 'home';
+	}
+?>
+</body>
+</html>
