6bdfd74cfcecf0e3b300b40feec3311d3220ded5
Jonathan Hosea <jonathan.hosea@me.com>
2014-10-18 23:14:58 +0700

    uygf

--- CMS/Class/cms.php
+++ CMS/Class/cms.php
@@ -16,12 +16,12 @@
     $q = "SELECT * FROM testDB ORDER BY created DESC LIMIT 3";
     $r = mysql_query($q);
 
-    if ( $r !== false && mysql_num_rows($r) > 0 ) {
+   
       while ( $a = mysql_fetch_assoc($r) ) {
         $title = stripslashes($a['title']);
         $bodytext = stripslashes($a['bodytext']);
 
-        $entry_display .= <<<ENTRY_DISPLAY
+        $entry_display = <<<ENTRY_DISPLAY
 
     <div class="post">
     	<h2>
@@ -34,25 +34,6 @@
 
 ENTRY_DISPLAY;
       }
-    } else {
-      $entry_display = <<<ENTRY_DISPLAY
-
-    <h2> This Page Is Under Construction </h2>
-    <p>
-      No entries have been made on this page. 
-      Please check back soon, or click the
-      link below to add an entry!
-    </p>
-
-ENTRY_DISPLAY;
-    }
-    $entry_display .= <<<ADMIN_OPTION
-
-    <p class="admin_link">
-      <a href="{$_SERVER['PHP_SELF']}?admin=1">Add a New Entry</a>
-    </p>
-
-ADMIN_OPTION;
 
     return $entry_display;
   }
